5f4065f71361c6bcb16a6d060cd5d3ac9828a299
bibek <pantabibek03@gmail.com>
2019-12-22 13:51:23 +0545

    Initial commit

---
+++ app/src/main/java/com/bibek/topic8/api/EmployeeAPI.java
@@ -0,0 +1,13 @@
+package com.bibek.topic8.api;
+
+import com.bibek.topic8.model.Employee;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface EmployeeAPI {
+    @GET("employees")
+    Call<List<Employee>> getAllEmployees();
+}
---
+++ app/src/main/java/com/bibek/topic8/url/url.java
@@ -0,0 +1,5 @@
+package com.bibek.topic8.url;
+
+public class url {
+    public  static final String base_url = "http://dummy.restapiexample.com/api/v1" ;
+}
